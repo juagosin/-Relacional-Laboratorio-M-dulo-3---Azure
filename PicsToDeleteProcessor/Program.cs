14026f3ac7b7a50afb6d8b4bc5cac5ed9b679dcf
juagosin <juagosin@gmail.com>
2025-01-28 18:35:12 +0100

    Creacion de los ejercicios obligatorios del laboratório relacional, módulo 4 azure

---
+++ PicsToDeleteProcessor/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Text.Json;
+using System.Threading;
+using Azure.Storage.Blobs;
+using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+
+
+Console.WriteLine("Hello to the QueueProcessor!");
+
+var queueClient = new QueueClient(Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING"), "pics-to-delete");
+
+queueClient.CreateIfNotExists();
+
+while (true)
+{
+    QueueMessage message = queueClient.ReceiveMessage();
+
+    if (message != null)
+    {
+        Console.WriteLine($"Message received {message.Body}");
+
+        var task = JsonSerializer.Deserialize<Task>(message.Body);
+
+        Console.WriteLine($"Let's delete images of {task.heroName} and {task.alterEgoName}");
+
+        if (task.heroName != null)
+        {
+            //Create a Blob service client
+            var blobClient = new BlobServiceClient(Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING"));
+
+            //Get container client
+            BlobContainerClient heroContainer = blobClient.GetBlobContainerClient("heroes");
+            BlobContainerClient alterEgoContainer = blobClient.GetBlobContainerClient("alteregos");
+
+            //Get blob with old name
+            var heroFileName = $"{task.heroName.Replace(' ', '-').ToLower()}.jpeg";
+            var alterEgoFileName = $"{task.alterEgoName.Replace(' ', '-').ToLower()}.png";
+            Console.WriteLine($"Looking for {heroFileName}");
+            var heroBlob = heroContainer.GetBlobClient(heroFileName);
+
+            if (heroBlob.Exists())
+            {
+                Console.WriteLine("Found it!");
+                Console.WriteLine($"Deleting {heroFileName}");
+
+
+                //Delete the old blob
+                heroBlob.DeleteIfExists();
+
+            }
+            else
+            {
+                Console.WriteLine($"There is no hero image to delete.");
+                Console.WriteLine($"Dismiss task.");
+                //Delete message from the queue
+                queueClient.DeleteMessage(message.MessageId, message.PopReceipt);
+            }
+            Console.WriteLine($"Looking for {alterEgoFileName}");
+            var alterEgoBlob = alterEgoContainer.GetBlobClient(alterEgoFileName);
+
+            if (alterEgoBlob.Exists())
+            {
+                Console.WriteLine("Found it!");
+                Console.WriteLine($"Deleting {alterEgoFileName}");
+
+
+                //Delete the old blob
+                alterEgoBlob.DeleteIfExists();
+
+                //Delete message from the queue
+                queueClient.DeleteMessage(message.MessageId, message.PopReceipt);
+            }
+            else
+            {
+                Console.WriteLine($"There is no alterEgo image to delete.");
+                Console.WriteLine($"Dismiss task.");
+                //Delete message from the queue
+                queueClient.DeleteMessage(message.MessageId, message.PopReceipt);
+            }
+
+        }
+        else
+        {
+            Console.WriteLine($"Bad message. Delete it");
+            //Delete message from the queue
+            queueClient.DeleteMessage(message.MessageId, message.PopReceipt);
+
+        }
+    }
+    else
+    {
+        Console.WriteLine($"Let's wait 5 seconds");
+        Thread.Sleep(5000);
+    }
+
+}
+
+class Task
+{
+    public string heroName { get; set; }
+    public string alterEgoName { get; set; }
+}
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# -Relacional-Laboratorio-M-dulo-3---Azure
+# Laboratorio Módulo 4 - Azure
+
+App ejecutándose en localhost:
+<img src="images/AppCorriendoEnLocalhost.png" />
+
+Por otro lado ejecutamos la cola pics-to-delete y vemos que permanece a la escucha:
+
+<img src="images/ColaEscuchando.png" />
+
+Procedemos al borrado del heroe DareDevil:
+
+<img src="images/BorradoHeroe.png" />
+
+
+Observamos que aparece el mensaje en la cola y procede al borrado de las imagenes ( el heroe lo busca por jpeg y el alterego por png)
+
+<img src="images/MensajeColaBorradoHeroeAlterEgo.png" />
